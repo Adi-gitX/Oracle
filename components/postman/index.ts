02b135e6f4d49260c9c3bf729c4b7def1af62871
adigitX <adityakammati.workspace@gmail.com>
2026-01-19 01:10:30 +0530

    add postman component exports

---
+++ components/postman/index.ts
@@ -0,0 +1,8 @@
+// Barrel export for postman components
+export { default as PostmanMode } from './PostmanMode'
+export { default as PostmanInput } from './PostmanInput'
+export { default as ResponseViewer } from './ResponseViewer'
+export { default as HeadersEditor } from './HeadersEditor'
+export { default as ParamsEditor } from './ParamsEditor'
+export { default as AuthEditor } from './AuthEditor'
+export { default as BodyEditor } from './BodyEditor'
